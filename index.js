f859f6ed24e28667e17224d4ccfb74e3145ea0da
maq2007 <mayanqureshi6@gmail.com>
2024-03-26 15:55:40 +0500

    added a random number generater  and published on npm

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 // 1)computer will generate a random number - Done
 // 2)user input for guessing number - Done
 // 3)compare user input and computer generated number and show results - 
-let randomNumber = 13;
+let randomNumber = Math.floor(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([
     {
-        message: "please guess a number: ",
+        message: "please guess a number between 1-10: ",
         type: "number",
         name: "userGuessedNumber",
     }
 ]);
 if (answers.userGuessedNumber === randomNumber) {
-    console.log('Congratulations you gessed the wrong number');
+    console.log('Congratulations you gessed the write number');
 }
 else {
     console.log('You guessed the wrong number');
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 
 // 1)computer will generate a random number - Done
@@ -6,17 +7,17 @@ import inquirer from "inquirer";
 
 // 3)compare user input and computer generated number and show results - 
 
-let randomNumber = 13;
+let randomNumber = Math.floor(Math.random() * 6 + 1);
 
 const answers = await inquirer.prompt([
     {
-        message: "please guess a number: ",
+        message: "please guess a number between 1-10: ",
         type: "number",
         name: "userGuessedNumber",
     }
 ])
 if(answers.userGuessedNumber === randomNumber){
-    console.log('Congratulations you gessed the wrong number');
+    console.log('Congratulations you geussed the correct number');
 }else{
     console.log('You guessed the wrong number')
 }
