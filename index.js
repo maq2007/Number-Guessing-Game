8572abe30fcd9b633fa60dec72027afd98b586f9
maq2007 <mayanqureshi6@gmail.com>
2024-03-26 14:38:15 +0500

    added the logic of the game

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 import inquirer from "inquirer";
-
 // 1)computer will generate a random number - Done
-
 // 2)user input for guessing number - Done
-
 // 3)compare user input and computer generated number and show results - 
-
 let randomNumber = 13;
-
 const answers = await inquirer.prompt([
     {
         message: "please guess a number: ",
         type: "number",
         name: "userGuessedNumber",
     }
-])
-if(answers.userGuessedNumber === randomNumber){
+]);
+if (answers.userGuessedNumber === randomNumber) {
     console.log('Congratulations you gessed the wrong number');
-}else{
-    console.log('You guessed the wrong number')
+}
+else {
+    console.log('You guessed the wrong number');
 }
